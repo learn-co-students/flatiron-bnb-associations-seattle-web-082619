9e06d65167815a6d3a0593691873a104a365dad8
Kara Odle <karajo.odle@gmail.coml>
2019-09-08 16:49:11 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,21 @@ class User < ActiveRecord::Base
   has_many :reservations, through: :listings
 
   has_many :reviews, :foreign_key => "guest_id"
+  has_many :trips, :class_name => "Reservation", :foreign_key => 'guest_id'
+
   # has_many :reservations, through: :reviews
 
-  def trips
-    Reservation.all
-
-    #this passes test but cannot be correct code, will compare w solution
-
-    #.collect do |review|
-    #  review.reservation
-    #end
-
-
-    #Reservation.all.select {|res| res.guest_id == self.id }
+  # def trips
+  #   Reservation.all
+  #
+  #   #this passes test but cannot be correct code, will compare w solution
+  #
+  #   #.collect do |review|
+  #   #  review.reservation
+  #   #end
+  #
+  #
+  #   #Reservation.all.select {|res| res.guest_id == self.id }
 
 
   end
